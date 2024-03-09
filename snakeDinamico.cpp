2131fea81b21fe7b3d1b89ac753dd3e631e6787f
Muners24 <ramon_chunqui@outlook.com>
2024-03-09 09:04:15 -0800

    m

--- snakeDinamico.cpp
+++ snakeDinamico.cpp
@@ -1,29 +1,39 @@
-
-
 #include "C:\raylib\raylib\src\raylib.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <conio.h>
 #include <time.h>
 #include <ctype.h>
 #include <stdbool.h>
 
-// resolucion
+// resolucion DEBE SER MULTIPLO DE CUADRICULA
 #define RANCHO 1080
 #define RALTO 720
+
 // divisiones
 #define CUADRICULA 40
-#define FILAS RALTO / CUADRICULA
-#define COLUMNAS RANCHO / CUADRICULA
+#define FILAS RALTO / CUADRICULA - BORDE_Y
+#define COLUMNAS RANCHO / CUADRICULA - BORDE_X
 #define CASILLAS FILAS *COLUMNAS
+// bordes
+// 1 cada lado
+#define BORDE_X 2
+// 2 arriba 1 abajo
+#define BORDE_Y 3
+#define BORDE_UP CUADRICULA * 2
+#define BORDE_RIGHT RANCHO - CUADRICULA
+#define BORDE_LEFT CUADRICULA
+#define BORDE_DOWN RALTO - CUADRICULA
 
-#define CONTORNO 4
-#define AJUSTEFRUTA CONTORNO / 2 + 1
-#define TAMFRUTA CUADRICULA - CONTORNO * 2
-//
-#define VELOCIDADSNK 2
+// fruta
+#define AJUSTEFRUTA 4
+#define TAMFRUTA CUADRICULA - AJUSTEFRUTA * 2
 
-// dierecciones
+// velocidad DEBE SER MULTIPLO DE CUADRICULA
+#define VELOCIDADSNK 4
+
+// direcciones
 #define DERECHA 0
 #define ARRIBA 1
 #define IZQUIERDA 2
@@ -31,79 +41,125 @@
 
 #define INDETERMINADO -1
 
-typedef Rectangle Trec;
+// puntuacion
+#define SCORE 20
 
-typedef struct _snake
+typedef Rectangle rec_t;
+
+typedef struct
 {                  //                              1
-    Trec pos;      //
+    rec_t pos;     //
     int direccion; //---------------->     2       p       0
                    //
                    //                              3
     int anteriorDir;
-} Tsnake;
+} snake_t;
 
 typedef struct _nodo
 {
-    Tsnake snake;
+    snake_t snake;
     struct _nodo *siguiente;
-} Tnodo;
+} nodo_t;
 
-typedef Tnodo *Tlista;
+typedef nodo_t *lista_t;
 
-typedef struct _fruta
+typedef struct
 {
-    Trec pos;
-    bool status;
-} Tfruta;
+    rec_t pos;
+    bool b_status;
+} fruta_t;
 
 char input(char tecla);
-void draw(Tlista lista, Tfruta fruta);
-void movimientoSnake(Tlista &lista, char tecla);
-char colisiones(Tlista &lista, Tfruta &fruta, char tecla);
-bool colisionSnake(Tsnake snake, Trec hitbox);
-bool colisionPared(Tsnake snake);
-Vector2 cabezaSnake(Tsnake snake);
+void draw(lista_t lista, fruta_t fruta, bool b_inicio,bool b_ganar, int score);
+void movimiento_snake(lista_t &lista, char tecla);
+char colisiones(lista_t &lista, fruta_t &fruta,char tecla, int &pieza_c);
+bool colision_snake(snake_t snake, rec_t hitbox);
+bool colision_pared(snake_t snake);
+Vector2 cabeza_snake(snake_t snake);
 
 int main(void)
 {
     InitWindow(RANCHO, RALTO, "window");
     SetTargetFPS(60);
     srand(time(NULL));
-
     int i;
+    int pieza_c;
+    char tecla;
 
-    char tecla = '0';
+    bool b_fin;
+    bool b_ganar;
+    bool b_inicio;
 
-    Tnodo *cabeza = (Tnodo *)malloc(sizeof(Tnodo));
-    Tlista lista = cabeza;
-    // inicializa nodo 1
-    cabeza->snake.pos.x = CUADRICULA * 10;
-    cabeza->snake.pos.y = CUADRICULA * 10;
-    cabeza->snake.pos.height = CUADRICULA;
-    cabeza->snake.pos.width = CUADRICULA;
-    cabeza->snake.direccion = INDETERMINADO;
-    cabeza->snake.anteriorDir = INDETERMINADO;
-    cabeza->siguiente = NULL;
+    nodo_t *cabeza;
 
-    Tfruta fruta;
-    fruta.status = true;
-    fruta.pos.x = CUADRICULA * (rand() % COLUMNAS) + 1 + AJUSTEFRUTA;
-    fruta.pos.y = CUADRICULA * (rand() % FILAS) + 1 + AJUSTEFRUTA;
-    fruta.pos.height = TAMFRUTA;
-    fruta.pos.width = TAMFRUTA;
+    lista_t lista;
 
-    while (tecla != 'x')
+    fruta_t fruta;
+    do
     {
+        pieza_c = 0;
+        tecla = '0';
+        
+        b_fin = false;
+        b_ganar=false;
+        b_inicio = false;
+        cabeza = (nodo_t *)malloc(sizeof(nodo_t));
 
-        draw(lista, fruta);
+        lista = cabeza;
+        // inicializa nodo 1
+        cabeza->snake.pos.x = CUADRICULA * 10;
+        cabeza->snake.pos.y = CUADRICULA * 10;
+        cabeza->snake.pos.height = CUADRICULA;
+        cabeza->snake.pos.width = CUADRICULA;
+        cabeza->snake.direccion = INDETERMINADO;
+        cabeza->snake.anteriorDir = INDETERMINADO;
+        cabeza->siguiente = NULL;
 
-        tecla = input(tecla);
+        fruta.b_status = true;
+        fruta.pos.x = CUADRICULA * (rand() % (COLUMNAS)) + AJUSTEFRUTA + CUADRICULA;
+        fruta.pos.y = CUADRICULA * (rand() % (FILAS)) + AJUSTEFRUTA + CUADRICULA * 2;
+        fruta.pos.height = TAMFRUTA;
+        fruta.pos.width = TAMFRUTA;
 
-        movimientoSnake(lista, tecla);
+        do
+        {
 
-        tecla = colisiones(lista, fruta, tecla);
-    }
+            draw(lista, fruta, b_inicio, b_ganar, pieza_c);
 
+            tecla = input(tecla);
+
+            if ('p' == tecla)
+            {
+                b_inicio = true;
+            }
+
+            if (b_inicio)
+            {
+                if(!b_fin)
+                {
+                    if(!b_ganar)
+                    {
+                        movimiento_snake(lista, tecla);
+                    }
+                    tecla = colisiones(lista, fruta, tecla,pieza_c);
+                }
+            }
+
+            if('x'==tecla)
+            {
+                b_fin = true;
+            }
+
+            if (5 == pieza_c)
+            {
+                b_ganar = true;
+                tecla= 'x';
+                b_fin= false;
+            }
+            
+        }while ('x' != tecla);
+    } while (!b_fin);
+    
     CloseWindow();
 
     return 0;
@@ -144,54 +200,102 @@ char input(char tecla)
     return tecla;
 }
 
-void draw(Tlista lista, Tfruta fruta)
+void draw(lista_t lista, fruta_t fruta, bool b_inicio,bool b_ganar, int score)
 {
     int i;
-    Tlista indice = lista;
+    lista_t indice = lista;
 
     BeginDrawing();
-
-    for (i = 0; i < RANCHO; i++)
-    {
-        if (i % CUADRICULA == 0)
-        {
-            DrawLine(i, 0, i, RALTO, WHITE);
-        }
-    }
-
-    for (i = 0; i < RALTO; i++)
-    {
-        if (i % CUADRICULA == 0)
-        {
-            DrawLine(0, i, RANCHO, i, WHITE);
-        }
-    }
-
     ClearBackground(BLACK);
 
-    // snake
-    DrawRectangleRec(indice->snake.pos, GREEN);
-    while (indice->siguiente != NULL)
+    // HORIZONTALES
+    for (i = BORDE_UP; i <= BORDE_DOWN; i++)
     {
-        indice = indice->siguiente;
-        DrawRectangleRec(indice->snake.pos, GREEN);
+        if (i % CUADRICULA == 0)
+        {
+            DrawLine(BORDE_LEFT, i, BORDE_RIGHT, i, WHITE);
+        }
     }
 
-    //fruta
+    // verticales
+    for (i = BORDE_LEFT; i <= BORDE_RIGHT; i++)
+    {
+        if (i % CUADRICULA == 0)
+        {
+            DrawLine(i, BORDE_UP, i, BORDE_DOWN, WHITE);
+        }
+    }
+
+    // snake
+    rec_t rec;
+    rec = indice->snake.pos;
+    rec.x += 1;
+    rec.y += 1;
+    rec.width -= 2;
+    rec.height -= 2;
+    DrawRectangleRec(indice->snake.pos, GREEN);
+    DrawRectangleRec(rec, GREEN);
+    while (NULL != indice->siguiente)
+    {
+        indice = indice->siguiente;
+        rec = indice->snake.pos;
+        rec.x += 1;
+        rec.y += 1;
+        rec.width -= 2;
+        rec.height -= 2;
+        DrawRectangleRec(indice->snake.pos, GREEN);
+        DrawRectangleRec(rec, GREEN);
+    }
+
+    // fruta
     DrawRectangleRec(fruta.pos, RED);
 
+    //mensajes
+    if (!b_inicio)
+    {
+        rec_t control_inicio;
+        control_inicio.width = CUADRICULA * 7;
+        control_inicio.height = CUADRICULA * 6;
+        control_inicio.x = BORDE_LEFT + CUADRICULA;
+        control_inicio.y = BORDE_UP + CUADRICULA;
+        DrawRectangleRec(control_inicio, DARKGRAY);
+        DrawText("\n\t\t  Controles\n\n\t\t\t\t    w\n\n\t\t\t  a\ts\td", control_inicio.x, control_inicio.y, 30, WHITE);
+        DrawText("\n\n\n\n\t\tP para iniciar\n\n\n\t\t X para salir",control_inicio.x-5, control_inicio.y+60, 30, WHITE);
+    }
+    else
+    {
+        // puntuacion
+        char text[50];
+        char txtscore[50];
+        itoa(score * SCORE, txtscore, 10);
+        strcpy(text, "Score: ");
+        strcat(text, txtscore);
+        DrawText(text, CUADRICULA, CUADRICULA, 30, WHITE);
+    }
+
+    if(b_ganar)
+    {
+        rec_t control_inicio;
+        control_inicio.width = CUADRICULA * 8;
+        control_inicio.height = CUADRICULA * 5;
+        control_inicio.x = BORDE_LEFT + CUADRICULA;
+        control_inicio.y = BORDE_UP + CUADRICULA;
+        DrawRectangleRec(control_inicio, DARKGRAY);
+        DrawText("\n\t\t\t Felicidades\n\n\t\t\t   Ganaste\n\n\n\t\t ", control_inicio.x, control_inicio.y, 30, WHITE);
+        DrawText("\n\n\t   P para repetir\n\n\n\t    X para salir",control_inicio.x-5, control_inicio.y+60, 30, WHITE);
+    }
     EndDrawing();
 }
 
-void movimientoSnake(Tlista &lista, char tecla)
+void movimiento_snake(lista_t &lista, char tecla)
 {
-    bool mov = true;
-    Tlista indice = lista;
-    static bool cruzarX = false;
-    static bool cruzarY = false;
+    bool b_mov = true;
+    lista_t indice = lista;
+    static bool b_cruzarX = false;
+    static bool b_cruzarY = false;
 
-    int(lista->snake.pos.y) % CUADRICULA == 0 ? cruzarY = true : cruzarY = false;
-    int(lista->snake.pos.x) % CUADRICULA == 0 ? cruzarX = true : cruzarX = false;
+    int(lista->snake.pos.y) % CUADRICULA == 0 ? b_cruzarY = true : b_cruzarY = false;
+    int(lista->snake.pos.x) % CUADRICULA == 0 ? b_cruzarX = true : b_cruzarX = false;
 
     // direccion de cabeza
     switch (tecla)
@@ -199,9 +303,9 @@ void movimientoSnake(Tlista &lista, char tecla)
     case 'd':
         if (lista->snake.direccion != IZQUIERDA)
         {
-            if (cruzarY)
+            if (b_cruzarY)
             {
-                if (lista->snake.direccion != INDETERMINADO)
+                if (INDETERMINADO != lista->snake.direccion)
                 {
                     lista->snake.anteriorDir = lista->snake.direccion;
                 }
@@ -212,9 +316,9 @@ void movimientoSnake(Tlista &lista, char tecla)
     case 'w':
         if (lista->snake.direccion != ABAJO)
         {
-            if (cruzarX)
+            if (b_cruzarX)
             {
-                if (lista->snake.direccion != INDETERMINADO)
+                if (INDETERMINADO != lista->snake.direccion)
                 {
                     lista->snake.anteriorDir = lista->snake.direccion;
                 }
@@ -225,9 +329,9 @@ void movimientoSnake(Tlista &lista, char tecla)
     case 'a':
         if (lista->snake.direccion != DERECHA)
         {
-            if (cruzarY)
+            if (b_cruzarY)
             {
-                if (lista->snake.direccion != INDETERMINADO)
+                if (INDETERMINADO != lista->snake.direccion)
                 {
                     lista->snake.anteriorDir = lista->snake.direccion;
                 }
@@ -238,9 +342,9 @@ void movimientoSnake(Tlista &lista, char tecla)
     case 's':
         if (lista->snake.direccion != ARRIBA)
         {
-            if (cruzarX)
+            if (b_cruzarX)
             {
-                if (lista->snake.direccion != INDETERMINADO)
+                if (INDETERMINADO != lista->snake.direccion)
                 {
                     lista->snake.anteriorDir = lista->snake.direccion;
                 }
@@ -253,14 +357,14 @@ void movimientoSnake(Tlista &lista, char tecla)
         break;
     }
 
-    Tlista anterior = indice;
+    lista_t anterior = indice;
     // direccion cuerpo
     while (indice->siguiente != NULL)
     {
         indice = indice->siguiente;
-        if (cruzarX)
+        if (b_cruzarX)
         {
-            if (cruzarY)
+            if (b_cruzarY)
             {
                 indice->snake.anteriorDir = indice->snake.direccion;
                 indice->snake.direccion = anterior->snake.anteriorDir;
@@ -272,7 +376,7 @@ void movimientoSnake(Tlista &lista, char tecla)
     indice = lista;
     // movimiento snake
 
-    while (mov)
+    while (b_mov)
     {
 
         if (indice->snake.direccion != INDETERMINADO)
@@ -293,37 +397,53 @@ void movimientoSnake(Tlista &lista, char tecla)
                 break;
             }
         }
-        if (indice->siguiente != NULL)
+        if (NULL != indice->siguiente)
         {
             indice = indice->siguiente;
         }
         else
         {
-            mov = false;
+            b_mov = false;
         }
     }
 }
 
-char colisiones(Tlista &lista, Tfruta &fruta, char tecla)
+char colisiones(lista_t &lista, fruta_t &fruta, char tecla, int &pieza_c)
 {
-    Tlista indice = lista;
+    lista_t indice = lista;
+    bool b_recalcular = false;
     // colision con fruta
-    if (colisionSnake(indice->snake, fruta.pos))
+    if (colision_snake(indice->snake, fruta.pos))
     {
         // posicion fruta
-        fruta.pos.x = CUADRICULA * (rand() % COLUMNAS) + 1 + AJUSTEFRUTA;
-        fruta.pos.y = CUADRICULA * (rand() % FILAS) + 1 + AJUSTEFRUTA;
+        do
+        {
+            b_recalcular = false;
+            fruta.pos.x = CUADRICULA * (rand() % (COLUMNAS)) + AJUSTEFRUTA + CUADRICULA;
+            fruta.pos.y = CUADRICULA * (rand() % (FILAS)) + AJUSTEFRUTA + CUADRICULA * 2;
+
+            while (NULL != indice->siguiente)
+            {
+                indice = indice->siguiente;
+
+                if (CheckCollisionRecs(fruta.pos, indice->snake.pos))
+                {
+                    b_recalcular = true;
+                    printf("recalculo pos frtura\n\n");
+                }
+            }
+        } while (b_recalcular);
 
         // generar e inicializar pieza de cuerpo
-        Tnodo *nuevo = (Tnodo *)malloc(sizeof(Tnodo));
+        nodo_t *nuevo = (nodo_t *)malloc(sizeof(nodo_t));
         if (nuevo)
         {
             nuevo->snake.pos.height = CUADRICULA;
             nuevo->snake.pos.width = CUADRICULA;
             nuevo->siguiente = NULL;
-
+            pieza_c++;
             // posicion e ingreso a la lista
-            while (indice->siguiente != NULL)
+            while (NULL != indice->siguiente)
             {
                 indice = indice->siguiente;
             }
@@ -354,20 +474,20 @@ char colisiones(Tlista &lista, Tfruta &fruta, char tecla)
     }
 
     indice = lista;
-    // colision con cuerpo
 
-    while (indice->siguiente != NULL)
+    // colision con cuerpo
+    while (NULL != indice->siguiente)
     {
         indice = indice->siguiente;
 
-        if (colisionSnake(lista->snake, indice->snake.pos))
+        if (colision_snake(lista->snake, indice->snake.pos))
         {
             return 'x';
         }
     }
 
     // colision con pared
-    if (colisionPared(lista->snake))
+    if (colision_pared(lista->snake))
     {
         return 'x';
     }
@@ -375,13 +495,13 @@ char colisiones(Tlista &lista, Tfruta &fruta, char tecla)
     return tecla;
 }
 
-bool colisionSnake(Tsnake snake, Trec hitbox)
+bool colision_snake(snake_t snake, rec_t hitbox)
 {
-    Vector2 cabeza = cabezaSnake(snake);
+    Vector2 cabeza = cabeza_snake(snake);
     return (CheckCollisionPointRec(cabeza, hitbox));
 }
 
-Vector2 cabezaSnake(Tsnake snake)
+Vector2 cabeza_snake(snake_t snake)
 {
     Vector2 cabeza;
     switch (snake.direccion)
@@ -406,31 +526,31 @@ Vector2 cabezaSnake(Tsnake snake)
     return cabeza;
 }
 
-bool colisionPared(Tsnake snake)
+bool colision_pared(snake_t snake)
 {
-    Vector2 cabeza = cabezaSnake(snake);
+    Vector2 cabeza = cabeza_snake(snake);
     switch (snake.direccion)
     {
     case DERECHA:
-        if (cabeza.x > RANCHO)
+        if (cabeza.x > BORDE_RIGHT)
         {
             return true;
         }
         break;
     case ARRIBA:
-        if (cabeza.y < 0)
+        if (cabeza.y < BORDE_UP)
         {
             return true;
         }
         break;
     case IZQUIERDA:
-        if (cabeza.x < 0)
+        if (cabeza.x < BORDE_LEFT)
         {
             return true;
         }
         break;
     case ABAJO:
-        if (cabeza.y > RALTO)
+        if (cabeza.y > BORDE_DOWN)
         {
             return true;
         }
